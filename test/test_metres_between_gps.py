024d659aaa55c223a3ca1e0dfd898ac46eeb67c4
Brian Henry <BrianHenryIE@gmail.com>
2019-03-15 23:51:51 -0700

    test updated

--- test/test_metres_between_gps.py
+++ test/test_metres_between_gps.py
@@ -6,19 +6,26 @@ class MetresBetweenGpsTests(unittest.TestCase):
 
     def test_citypark_to_nearest(self):
 
-        from_gps = {
+        city_park_gps = {
+            "longitude": -121.49305556,
+            "latitude": 38.58638889
+        }
+
+        e_and_ninth_gps = {
             "longitude": -121.4920789003372,
             "latitude": 38.586078791838744
         }
 
-        to_gps = {
-            "longitude": -121.4920789003372,
-            "latitude": 38.586078791838744
-        }
+        # http://www.cqsrg.org/tools/GCDistance/
+        # 0.091794 km
 
-        expected = 110
+        # https://www.google.com/maps/dir/'38.58638889,-121.49305556'/'38.586078791838744,-121.4920789003372'/@38.5863537,-121.4931581,18.66z/data=!4m10!4m9!1m3!2m2!1d-121.4930556!2d38.5863889!1m3!2m2!1d-121.4920789!2d38.5860788!3e3
+        # 299 ft  ...121m
 
-        actual = metres_between_gps(from_gps, to_gps)
+        expected = 105
+        delta = 20
 
-        self.assertEqual(expected, actual)
+        actual = metres_between_gps(city_park_gps, e_and_ninth_gps)
+
+        self.assertTrue(expected - delta <= actual <= expected + delta)
 
