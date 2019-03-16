642c3ea75093c808fe9cd9d12ccf2cccece27813
Brian Henry <BrianHenryIE@gmail.com>
2019-03-16 00:22:12 -0700

    Notes added to test

--- test/test_nearest_station_to_gps.py
+++ test/test_nearest_station_to_gps.py
@@ -6,6 +6,13 @@ class NearestStationToGpsTests(unittest.TestCase):
 
     def test_nearest_station(self):
 
+        # 10 m difference according to Google:
+        # https://www.google.com/maps/dir/'38.5863,-121.4930'/'38.58625,-121.4929'/@38.5863537,-121.4931581,18z/data=!4m10!4m9!1m3!2m2!1d-121.493!2d38.5863!1m3!2m2!1d-121.4929!2d38.58625!3e3
+
+        # https://www.wolframalpha.com/input/?i=distance+between+(38.5863,-121.4930),(38.58625,-121.4929)
+        # (38.5863,-121.4930),(38.58625,-121.4929)
+        # 0.000111
+
         gps = {
             "longitude": -121.4920789003372,
             "latitude": 38.586078791838744
