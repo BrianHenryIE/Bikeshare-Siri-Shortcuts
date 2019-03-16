991c34d26d3808f6d5e7a6e213963df271072c99
Brian Henry <BrianHenryIE@gmail.com>
2019-03-16 01:44:58 -0700

    Unit test updated

--- test/test_where_is_the_nearest_jump_bike.py
+++ test/test_where_is_the_nearest_jump_bike.py
@@ -30,12 +30,15 @@ class MyTests(unittest.TestCase):
         # 110m
 
         with open('data/free_bike_status.json', 'r') as f:
-            free_bike_status = json.load(f).data.bikes
+            free_bike_status = json.load(f)['data']['bikes']
+
+        with open('data/station_status.json', 'r') as f:
+            station_status = json.load(f)['data']['stations']
 
         with open('data/station_information.json', 'r') as f:
-            station_information = json.load(f).data.stations
+            station_information = json.load(f)['data']['stations']
 
-        expected = "The nearest bike is docked at E St & 9th St, 110 m east."
-        actual = where_is_the_nearest_jump_bike(free_bike_status, station_information, city_park_gps)
+        expected = "The nearest bike is docked at E St & 9th St, 92 m east."
+        actual = where_is_the_nearest_jump_bike(free_bike_status, station_status, station_information, city_park_gps)
 
         self.assertEqual(expected, actual)
